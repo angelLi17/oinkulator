5618938a5f32e3cd6f6e71f8e9c50fc0c5691f4e
angelLi17 <liangelfremont@gmail.com>
2025-03-15 14:10:49 -0700

    hi

--- offscreen.js
+++
@@ -1,7 +0,0 @@
-chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-    if (msg.action === 'play') {
-      const audio = new Audio(msg.url);
-      audio.play();
-      audio.onended = () => chrome.runtime.sendMessage({ action: 'audio-ended' });
-    }
-  });
