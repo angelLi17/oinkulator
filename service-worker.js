787626c3c600826754198e0bbb7f1e3c878e1461
jsguan2452 <jsguan2452@gmail.com>
2025-03-15 18:32:20 -0700

    hi

--- service-worker.js
+++ service-worker.js
@@ -11,17 +11,4 @@ chrome.scripting
     })
     .then(() => console.log("injected script file"));
 
-document.getElementById("1").addEventListener("click", display(1));
-document.getElementById("2").addEventListener("click", display(2));
-document.getElementById("3").addEventListener("click", display(3));
-document.getElementById("4").addEventListener("click", display(4));
-document.getElementById("5").addEventListener("click", display(5));
-document.getElementById("6").addEventListener("click", display(6));
-document.getElementById("7").addEventListener("click", display(7));
-document.getElementById("8").addEventListener("click", display(8));
-document.getElementById("9").addEventListener("click", display(9));
-document.getElementById("0").addEventListener("click", display(0));
 
-function display(a) {
-    document.getElementById("space1").innerHTML = a;
-}
