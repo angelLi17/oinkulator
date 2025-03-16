de1597aff2463fcb65b39f40952a0a2223f83a4f
jsguan2452 <jsguan2452@gmail.com>
2025-03-15 18:21:02 -0700

    hi

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-importScripts('script.js');
-=======
-import './sw-omnibox.js';
-
-console.log("sw-omnibox.js");
-
 function getTabId() {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     callback(tabs[0].id);
@@ -32,4 +25,3 @@ document.getElementById("0").addEventListener("click", display(0));
 function display(a) {
     document.getElementById("space1").innerHTML = a;
 }
->>>>>>> a4d75379bac3e4ce23faefcca9acba8cbf1d4e4a
