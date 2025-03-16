340380c41deb6f2eebbeebd23b515bf462f625a5
angelLi17 <liangelfremont@gmail.com>
2025-03-15 18:51:20 -0700

    sync

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 const audio = new Audio("assets/pig.mp3");
 const button = document.getElementById("playSoundButton");
 button.addEventListener("click", () => {
-    const timesToPlay = Math.floor(Math.random() * 5) + 1;
-  
-    let playCount = 0;
-
     audio.play();
   });
