47d911281901fbdf3e402d5d4575ada4c77e8726
jsguan2452 <jsguan2452@gmail.com>
2025-03-15 18:34:02 -0700

    hi

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,14 @@
-function getTabId() {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    callback(tabs[0].id);
-  });
-}
+// function getTabId() {
+//   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//     callback(tabs[0].id);
+//   });
+// }
 
-chrome.scripting
-    .executeScript({
-      target : {tabId : getTabId()},
-      files : [ "script.js" ],
-    })
-    .then(() => console.log("injected script file"));
+// chrome.scripting
+//     .executeScript({
+//       target : {tabId : getTabId()},
+//       files : [ "script.js" ],
+//     })
+//     .then(() => console.log("injected script file"));
 
 
