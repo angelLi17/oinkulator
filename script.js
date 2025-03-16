abf144d28fa77621bf0278fcd074c952030b156f
angelLi17 <liangelfremont@gmail.com>
2025-03-15 18:44:52 -0700

    help

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const audio = new Audio("assets/pig.mp3");
 const button = document.getElementById("playSoundButton");
 button.addEventListener("click", () => {
-    
-    audio.play(); // Play the sound when the button is clicked
+    const timesToPlay = Math.floor(Math.random() * 5) + 1;
+  
+    let playCount = 0;
+
+    audio.play();
+    playCount++;
+
+    if (playCount < timesToPlay) {
+    // Wait for the audio to finish before playing again
+    audio.onended = playAudio;
+    } else {
+      // Reset onended event after all plays are done
+      audio.onended = null;
+    }
   });
